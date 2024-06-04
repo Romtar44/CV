e4944e83009817052a309207f867dfbbb079ec05
Romtar44 <85120296+Romtar44@users.noreply.github.com>
2024-06-04 15:22:17 +0200

    test

--- Cv/src/components/image/image.tsx
+++ Cv/src/components/image/image.tsx
@@ -1,27 +1,30 @@
-import styles from "./image.module.scss"
+import styles from "./image.module.scss";
 
 type ImageProps = {
-	imgSrc: string,
-	alt: string,
-	w?: string,
-	h?: string,
-	border?: string,
-	className?: string
-}
+  imgSrc: string;
+  alt: string;
+  w?: string;
+  h?: string;
+  border?: string;
+  className?: string;
+};
 
-const Image = ({
-	imgSrc,
-	alt,
-	w = "100%",
-	h = "100%",
-	border = '1px solid black',
-	className,
-}: ImageProps) =>
-{
-	return (
-		<div className={`${styles.imgContainer} ${className}`} style={{ width: w, height: h, border: border }}>
-			<img src={imgSrc} alt={alt} />
-		</div>)
-}
+export const MyImage = ({
+  imgSrc,
+  alt,
+  w = "100%",
+  h = "100%",
+  border = "1px solid black",
+  className,
+}: ImageProps) => {
+  return (
+    <div
+      className={`${styles.imgContainer} ${className}`}
+      style={{ width: w, height: h, border: border }}
+    >
+      <img src={imgSrc} alt={alt} />
+    </div>
+  );
+};
 
-export default Image
+export default Image;
---
+++ Cv/src/components/utils/error.tsx
@@ -0,0 +1,35 @@
+import React, { Component, ReactNode, ErrorInfo } from 'react';
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(_: Error): ErrorBoundaryState {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    console.error('ErrorBoundary caught an error:', error, errorInfo);
+  }
+
+  render(): ReactNode {
+    console.log('coucou')
+    if (this.state.hasError) {
+      return <div style={{ backgroundColor: 'blue', width: '100%', height: '100%' }}>Image non disponible</div>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
